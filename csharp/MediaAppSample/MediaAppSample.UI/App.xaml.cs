30aec350ee8a73a825ba4b5a123f96b9986cc635
robcamer <robcamer@microsoft.com>
2017-02-19 10:25:38 -0500

    Updated to MediaPlayer Element.  Fixed call to turn off scaling on Xbox One.

--- csharp/MediaAppSample/MediaAppSample.UI/App.xaml.cs
+++ csharp/MediaAppSample/MediaAppSample.UI/App.xaml.cs
@@ -148,19 +148,7 @@ namespace MediaAppSample.UI
                         await Platform.Current.AppInitializingAsync(InitializationModes.Restore);
                 }
 
-                //Turn off Title Safe Area overscan adjustment
-                if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationViewBoundsMode") && Platform.DeviceFamily == DeviceFamily.Xbox)
-                {
-                    var AppView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
-                    AppView.SetDesiredBoundsMode(Windows.UI.ViewManagement.ApplicationViewBoundsMode.UseCoreWindow);
-                }
 
-                //Turn of scaling, if app is not designed at 540p (Xbox One default scaling is 200% for XAML, 150% for Web Programming model apps)
-                if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent(
-                        "Windows.UI.ViewManagement.ApplicationViewScaling"))
-                {
-                    Windows.UI.ViewManagement.ApplicationViewScaling.TrySetDisableLayoutScaling(true);
-                }
 
                 Frame rootFrame = Window.Current.Content as Frame;
 
--- csharp/MediaAppSample/MediaAppSample.UI/Views/MainView.xaml.cs
+++ csharp/MediaAppSample/MediaAppSample.UI/Views/MainView.xaml.cs
@@ -38,6 +38,20 @@ namespace MediaAppSample.UI.Views
         public MainView()
         {
             this.InitializeComponent();
+
+            //Turn off Title Safe Area overscan adjustment
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent("Windows.UI.ViewManagement.ApplicationViewBoundsMode") && Platform.DeviceFamily == DeviceFamily.Xbox)
+            {
+                var AppView = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+                AppView.SetDesiredBoundsMode(Windows.UI.ViewManagement.ApplicationViewBoundsMode.UseCoreWindow);
+            }
+
+            //Turn of scaling, if app is not designed at 540p (Xbox One default scaling is 200% for XAML, 150% for Web Programming model apps)
+            if (Windows.Foundation.Metadata.ApiInformation.IsTypePresent(
+                    "Windows.UI.ViewManagement.ApplicationViewScaling"))
+            {
+                Windows.UI.ViewManagement.ApplicationViewScaling.TrySetDisableLayoutScaling(true);
+            }
         }
 
         protected override Task OnLoadStateAsync(LoadStateEventArgs e)
